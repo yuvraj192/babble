c67b1e96ce13ff5e3e46a0d8c50d6106b59659fd
yuvraj192 <yuvrajverma192@gmail.com>
2020-07-09 15:06:01 +0530

    test

--- app/src/main/java/com/messaging/babble/MainActivity.kt
+++ app/src/main/java/com/messaging/babble/MainActivity.kt
@@ -1,13 +1,46 @@
 package com.messaging.babble
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(loginView!= null){
+            val webSettings = loginView!!.settings
+            loginView.settings.javaScriptEnabled = true
+            loginView!!.webViewClient=  WebViewClient()
+            loginView!!.webChromeClient=  WebChromeClient()
+
+            loginView.addJavascriptInterface(object : Any(){
+                @JavascriptInterface
+                fun performClick (PhoneNo: String){
+                    Toast.makeText(applicationContext ," hello World", Toast.LENGTH_LONG).show()
+                }
+            },"valid")
+
+
+
+            loginView!!.loadUrl("file:///android_asset/login.html")
+            loginView!!.webViewClient = object : WebViewClient(){
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?){
+                    super.onPageStarted(view,url,favicon)
+
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                }
+            }
+        }
     }}
 
-
-    //hello !!!VJMGJgjjljlj
