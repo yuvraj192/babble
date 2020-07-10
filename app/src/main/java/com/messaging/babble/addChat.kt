a5019ae0ebe2f803d2d00b9ca1c02bdeffb37675
yuvraj192 <yuvrajverma192@gmail.com>
2020-07-11 02:22:48 +0530

    test

--- app/src/main/java/com/messaging/babble/addChat.kt
+++ app/src/main/java/com/messaging/babble/addChat.kt
@@ -2,11 +2,18 @@ package com.messaging.babble
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_add_chat.*
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class addChat : AppCompatActivity() {
@@ -15,13 +22,40 @@ class addChat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_chat)
+
+        if(conView!= null){
+            val webSettings = conView!!.settings
+            conView.settings.javaScriptEnabled = true
+            conView!!.webViewClient=  WebViewClient()
+            conView!!.webChromeClient=  WebChromeClient()
+
+
+
+            conView!!.loadUrl("file:///android_asset/addChat.html")
+            conView!!.webViewClient = object : WebViewClient(){
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?){
+                    super.onPageStarted(view,url,favicon)
+
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                }
+            }
+        }
+
+        
+        
+        
+        
+        
         arrayList = ArrayList()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), 1)
         } else {
             getcontact()
         }
-    }
+
 
     private fun getcontact() {
         val cursor = contentResolver.query(
