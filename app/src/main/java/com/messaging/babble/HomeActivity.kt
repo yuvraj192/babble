d267f5705e3e0d4add802649e19f5c2f0215eb52
yuvraj192 <yuvrajverma192@gmail.com>
2020-07-10 17:24:28 +0530

    test

--- app/src/main/java/com/messaging/babble/HomeActivity.kt
+++ app/src/main/java/com/messaging/babble/HomeActivity.kt
@@ -3,9 +3,11 @@ package com.messaging.babble
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -24,14 +26,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
     private val socket: Socket = IO.socket("http://iotine.zapto.org:4600/")
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+    private val channelId = "com.messaging.notiex"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         socket.connect()
-        socket.emit("join", "divyanshg21")
+        socket.emit("join", "yuvj")
 
 
         if (homeView != null){
@@ -89,7 +96,7 @@ class HomeActivity : AppCompatActivity() {
 
         }
     private fun notifyMessage(msg: String, by: String, time: String){
-
+        sendnoti(msg)
     }
     private fun openActivity(){
         val intent = Intent(this@HomeActivity, ChatActivity::class.java)
@@ -99,6 +106,30 @@ class HomeActivity : AppCompatActivity() {
         val intent = Intent(this@HomeActivity, MainActivity::class.java)
         startActivity(intent)
 
+    }
+
+    private fun sendnoti(msg: String){
+        val pendingIntent = PendingIntent.getActivity(this,0,intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId,"test",NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(true)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(this,channelId)
+                .setSmallIcon(R.mipmap.logo_round)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.mipmap.logo))
+                .setContentIntent(pendingIntent)
+        }else{
+
+            builder = Notification.Builder(this)
+                .setSmallIcon(R.mipmap.logo_round)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.mipmap.logo))
+                .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(1234,builder.build())
     }
 
     override fun onDestroy() {
