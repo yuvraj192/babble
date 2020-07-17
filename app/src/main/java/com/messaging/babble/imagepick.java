784103f6ff0bd23e127aed7f6c638fc873966b78
dailyvines111 <dailyvines111@gmail.com>
2020-07-17 14:15:37 +0530

    test

---
+++ app/src/main/java/com/messaging/babble/imagepick.java
@@ -0,0 +1,14 @@
+package com.messaging.babble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class imagepick extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
